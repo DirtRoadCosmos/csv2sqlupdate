a6e8a15bc18f02c535d1d4feeb2fdb10eaae0bb8
andytilia <andytilia@users.noreply.github.com>
2014-10-24 14:42:42 -0400

    corrections and write output to file

--- csv2sqlupdate.py
+++ csv2sqlupdate.py
@@ -1,17 +1,42 @@
-import csv
+import csv, sys
 
 # process the arguments
-if len(sys.argv) != 2:
+print 'processing the arguments'
+
+def escape(string):
+    newString = []
+    for character in string:
+        if character == '\'':
+            newCharacter = '\'\''
+        else:
+            newCharacter = character
+        newString.append(newCharacter)
+    return ''.join(newString)
+    
+if len(sys.argv) != 3:
     sys.exit()
 else:
-    filename = sys.argv[1]
-    tablename = sys.arv[2]
+    fileName = sys.argv[1]
+    tableName = sys.argv[2]
 
-with open(filename, 'rb') as csvfile:
-    theReader = csv.reader(csvfile, delimiter=',')
+with open(fileName, 'rb') as theFile:
+    theReader = csv.reader(theFile, delimiter=',')
+    rownum = 0
+    theOutput = []
     for row in theReader:
-        thisOutput = ['UPDATE', tablename, 'SET', head1, '/= /'', row[0], '/'', 'WHERE', head2, '/= /'', row[1], '/';']
-        theOutput.append(''.join(thisOutput))
-        print thisOutput
+        if rownum == 0:
+            header = row
+            print header
+        else:
+            thisOutput = ''.join([
+                'UPDATE ', tableName, ' SET ', header[1], ' = \'', escape(row[2]),
+                '\' WHERE ', header[0], ' = \'', escape(row[0]),
+                '\' AND ', header[1], ' = \'', escape(row[1]), '\';\n'
+                ])
+            theOutput.append(thisOutput)
+            print thisOutput
+        rownum += 1
 
-        
+with open(fileName[:-4] + '_output.sql', 'w') as outputFile:
+    for item in theOutput:
+        outputFile.write(item)
